cbb93565413bdfcdb2340c2a260cd419b2a0852d
ayushhagarwal <ayushgrwl365@gmail.com>
2023-07-17 22:43:48 +0530

    feat: complete task functionality

--- src/app/components/tasks-form/tasks-form.component.ts
+++ src/app/components/tasks-form/tasks-form.component.ts
@@ -1,24 +1,28 @@
 import { Component, OnInit } from '@angular/core';
+import { Task } from '../../constants/tasks.interface';
 
 @Component({
   selector: 'app-tasks-form',
   templateUrl: './tasks-form.component.html'
 })
 export class TasksFormComponent implements OnInit {
-
+  
   constructor() { }
 
   ngOnInit(): void {
   }
-  tasks: string[] = []; 
+  tasks: Task[] = []; 
   newTask: string = ''; 
 
   addTask() {
-    if (this.newTask.trim() !== '') { 
-      this.tasks.push(this.newTask);
+    if (this.newTask.trim() !== '') {
+      const newTask: Task = {
+        name: this.newTask,
+        completed: false
+      };
+      this.tasks.push(newTask);
       this.newTask = '';
     }
-    console.log(this.tasks);
   }
 
 }
--- src/app/components/tasks-list/tasks-list.component.ts
+++ src/app/components/tasks-list/tasks-list.component.ts
@@ -1,20 +1,25 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { Task } from '../../constants/tasks.interface';
 
 @Component({
   selector: 'app-tasks-list',
   templateUrl: './tasks-list.component.html',
 })
 export class TasksListComponent implements OnInit {
-  @Input() tasks: string[];
+  @Input() tasks: Task[];
   constructor() { }
 
   ngOnInit(): void {
   }
 
-  removeTask(task: string) {
+  removeTask(task: Task) {
     const taskIndex = this.tasks.indexOf(task);
     if (taskIndex !== -1) {
       this.tasks.splice(taskIndex, 1);
     }
+  }
+
+  toggleCompleted(task: Task) {
+    task.completed = !task.completed;
   }
 }
